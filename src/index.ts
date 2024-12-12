b52793ecb5cac55ebcb772b3d70f98ce3b70a979
al3xpisani <alexandre.pisani.ant@gmail.com>
2024-12-12 14:15:08 -0300

    fixed express issues

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,15 @@ import bodyParser from "body-parser"
 
 const app = express()
 app.use(cors())
-// const port = process.env.EXPRESS_PORT
+const port = process.env.EXPRESS_PORT
 setRoutePaths(app)
 
 export const setBodyParser2Json = () => {
     return app.use(bodyParser.json())
 }
 
-// app.listen(port, () => {
-//      console.log(`Express is listening at http://localhost:${port}`)
-// })
+app.listen(port, () => {
+    console.log(`Express is listening at http://localhost:${port}`)
+})
 
 export default app
