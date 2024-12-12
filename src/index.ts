a281c7f942ba7bf7f7fdd59a26109e10a2807404
al3xpisani <alexandre.pisani.ant@gmail.com>
2024-12-12 14:06:33 -0300

    fixed express issues

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import bodyParser from "body-parser"
 
 const app = express()
 app.use(cors())
-const port = process.env.EXPRESS_PORT
+// const port = process.env.EXPRESS_PORT
 setRoutePaths(app)
 
 export const setBodyParser2Json = () => {
